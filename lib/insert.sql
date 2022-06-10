80e4ad0666c2e4388649f7053d820a6daf16861d
Mark <markwilkins86@gmail.com>
2022-06-10 13:21:34 -0400

    commit tree 78cddffefc56cefe26f2477cbb15d6c2c5ab3a9a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 8, "Male", "Brown", "Happy", true);
+INSERT INTO bears VALUES (2, "Rowdy", 8, "Male", "Black", "Sad", true);
+INSERT INTO bears VALUES (3, "Tabitha", 12, "Female", "White", "Melancoly", false);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 2, "Male", "Tan", "Silly", true);
+INSERT INTO bears VALUES (5, "Melissa", 14, "Female", "Grey", "Boring", false);
+INSERT INTO bears VALUES (6, "Grinch", 1, "Male", "Green", "Angry", true);
+INSERT INTO bears VALUES (7, "Wendy", 20, "Female", "Red", "Content", false);
+INSERT INTO bears VALUES (8, NULL, 13, "Male", "White", "Blah", true);
